d1b180654edd35743f668be804b24e58c12c7b47
jogp10 <jogp10@gmail.com>
2022-10-13 18:00:51 +0100

    Start llopen

--- code/src/link_layer.c
+++ code/src/link_layer.c
@@ -2,72 +2,63 @@
 
 #include "link_layer.h"
 
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <termios.h>
+#include <unistd.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+
 // MISC
 #define _POSIX_SOURCE 1 // POSIX compliant source
+#define FALSE 0
+#define TRUE 1
 
 struct termios oldtio;
 struct termios newtio;
-
 int fd;
+int alarmEnabled = FALSE;
+int alarmCount = 0;
+
+// Alarm function handler
+void alarmHandler(int signal)
+{
+    alarmEnabled = FALSE;
+    alarmCount++;
+    printf("Alarm #%d\n", alarmCount);
+}
 
 ////////////////////////////////////////////////
 // LLOPEN
 ////////////////////////////////////////////////
 int llopen(LinkLayer connectionParameters)
 {
-    // TODO
 
     const char *serialPortName = connectionParameters.serialPort;
     LinkLayerRole role = connectionParameters.role;
+    int nRetransmissions = connectionParameters.nRetransmissions;
+    int timeout = connectionParameters.timeout;
 
-    // Open serial port device for reading and writing, and not as controlling tty
-    // because we don't want to get killed if linenoise sends CTRL-C.
-    fd = open(serialPortName, O_RDWR | O_NOCTTY);
+    (void)signal(SIGALRM, alarmHandler);
 
-    if (fd < 0)
-    {
-        perror(serialPortName);
-        exit(-1);
-    }
-
-    // Save current port settings
-    if (tcgetattr(fd, &oldtio) == -1)
-    {
-        perror("tcgetattr");
-        exit(-1);
-    }
-
-    // Clear struct for new port settings
-    memset(&newtio, 0, sizeof(newtio));
-
-    newtio.c_cflag = connectionParameters.baudRate | CS8 | CLOCAL | CREAD;
-    newtio.c_iflag = IGNPAR;
-    newtio.c_oflag = 0;
-
-    // Set input mode (non-canonical, no echo,...)
-    newtio.c_lflag = 0;
-    newtio.c_cc[VTIME] = connectionParameters.timeout; // Inter-character timer
-    newtio.c_cc[VMIN] = 1;  // Blocking read until 1 chars received
-
-    // VTIME e VMIN should be changed in order to protect with a
-    // timeout the reception of the following character(s)
-
-    // Now clean the line and activate the settings for the port
-    // tcflush() discards data written to the object referred to
-    // by fd but not transmitted, or data received but not read,
-    // depending on the value of queue_selector:
-    //   TCIFLUSH - flushes data received but not read.
-    tcflush(fd, TCIOFLUSH);
-
-    // Set new port settings
-    if (tcsetattr(fd, TCSANOW, &newtio) == -1)
-    {
-        perror("tcsetattr");
-        exit(-1);
-    }
+    int fd = openSerialPort(serialPortName, oldtio, newtio);
 
     printf("New termios structure set\n");
 
+    if (role == LlTx)
+    {
+        //return llopen_tx(fd);
+    }
+    else
+    {
+        //return llopen_rx(fd);
+    }
+    
     return 1;
 }
 
